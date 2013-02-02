be388a04671d2df12186d369fec6e0fe6179e215
Nahuel Garbezza <n.garbezza@gmail.com>
2013-02-02 17:48:51 -0300

    new parser rspec matcher; first impl

--- spec/parse_matcher.rb
+++ spec/parse_matcher.rb
@@ -13,6 +13,9 @@ end
 
 RSpec::Matchers.define :be_parsed_to do |expected|
 
+  puts '!!!!!!! DEPRECATED !!!!!!!
+  use be_parsed_as(elem).(and_fail | and_return(expected))'
+
   include ParserMatchersHelper
 
   match_for_should do |actual|
@@ -42,6 +45,9 @@ end
 
 RSpec::Matchers.define :be_parsed_as_main_def_to do |expected|
 
+  puts '!!!!!!! DEPRECATED !!!!!!!
+  use be_parsed_as(elem).(and_fail | and_return(expected))'
+
   include ParserMatchersHelper
 
   match_for_should do |actual|
@@ -55,11 +61,55 @@ end
 
 RSpec::Matchers.define :fail_to_parse do
 
+  puts '!!!!!!! DEPRECATED !!!!!!!
+  use be_parsed_as(elem).(and_fail | and_return(expected))'
+
   include ParserMatchersHelper
 
   match do |actual|
     parse actual
     @node.nil?
+  end
+
+end
+
+RSpec::Matchers.define :be_parsed_as do |grammar_elem|
+
+  @valid_nodes = [:program, :procedure, :function,
+    :main, :expression, :command ]
+
+  chain :and_fail do
+    @expect_parser_results = false
+  end
+
+  chain :and_return do |expected|
+    @expect_parser_results = true
+    @expected = expected
+  end
+
+  match do |actual|
+    fail 'wrong expectation' if @expect_parser_results.nil?
+    fail 'grammar elem not supported' if !@valid_nodes.include?(grammar_elem)
+
+    parse send("#{grammar_elem}_code_to_program", actual)
+
+    if @expect_parser_results
+      @node.value == send("#{grammar_elem}_node_to_program", @expected)
+    else
+      @node.nil?
+    end
+  end
+
+  def program_code_to_program(code)
+    code
+  end
+
+  def program_node_to_program(node)
+    node
+  end
+
+  def parse(code)
+    @node = PARSER.parse code
   end
 
 end
