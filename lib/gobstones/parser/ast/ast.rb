bd65ebb3eab3861a937badd1e9e9ed8cb04f5d74
Nahuel Garbezza <n.garbezza@gmail.com>
2013-05-12 08:48:03 -0300

    best way to require all lang stuff

--- lib/gobstones/parser/ast/ast.rb
+++ lib/gobstones/parser/ast/ast.rb
@@ -1,9 +1,4 @@
-require 'gobstones/lang/program'
-require 'gobstones/lang/commands/command_block'
-require 'gobstones/lang/commands/poner_cmd'
-require 'gobstones/lang/commands/mover_cmd'
-require 'gobstones/lang/definitions/no_return'
-require 'gobstones/lang/definitions/main'
+require 'gobstones/lang/all'
 
 module Gobstones
 
