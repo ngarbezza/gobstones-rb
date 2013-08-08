7bf2d9e15352294811e3a9732c8376e9d97f7a60
Nahuel Garbezza <n.garbezza@gmail.com>
2013-08-07 23:50:19 -0300

    support for evaluating procedures with arguments

--- lib/gobstones/lang/definitions/procedure.rb
+++ lib/gobstones/lang/definitions/procedure.rb
@@ -1,5 +1,6 @@
 require 'gobstones/lang/definitions/definition'
 require 'gobstones/lang/definitions/no_return'
+require 'gobstones/runner/execution_context'
 
 module Gobstones
 
@@ -7,15 +8,16 @@ module Gobstones
 
     class Procedure < Definition
 
+      # TODO rename args to a better name, args_tuple?
+
       def initialize(name, args, body)
         super(name, args, body, NoReturn.new)
       end
 
       def evaluate(context, arguments=[])
         check_number_of_arguments arguments
-        procedure_context = ProcedureExecutionContext.based_on context
-        # TODO create new context and set variables
-        # args.variables.length.times { |i| context.set args.variables[i], arguments[i] }
+        procedure_context = Gobstones::Runner::ProcedureExecutionContext.based_on context
+        set_arguments arguments, procedure_context
         body.evaluate procedure_context
       end
 
@@ -25,6 +27,12 @@ module Gobstones
         if args.length != arguments.length
           message = "Wrong number of arguments in procedure '#{name}': expected #{args.length}, got #{arguments.length}"
           raise WrongArgumentsError, message
+        end
+      end
+
+      def set_arguments(arguments, procedure_context)
+        args.length.times do |index|
+          procedure_context.set args.variables[index], arguments[index]
         end
       end
 
--- spec/lang/commands/procedure_spec.rb
+++ spec/lang/commands/procedure_spec.rb
@@ -23,6 +23,31 @@ describe Procedure do
       .to raise_error(UndefinedVariableError)
   end
 
+  it "should set arguments in the new context and they can be used" do
+    a_color = VarName.new 'a_color'
+    a_direction = VarName.new 'a_direction'
+    args = VarTuple.new [a_color, a_direction]
+    mover_cmd = Mover.new a_direction
+    poner_cmd = Poner.new a_color
+    body = CmdBlock.new [mover_cmd, poner_cmd]
+    procedure = Procedure.new 'MyProc', args, body
+
+    procedure.evaluate program_context, [Negro.new, Norte.new]
+
+    program_context.head.are_there_balls?(Negro.new).should be_true
+    program_context.head.y_pos.should == 1
+  end
+
+  it "should not set arguments as var names in outer context" do
+    a_direction = VarName.new 'a_direction'
+    args = VarTuple.new [a_direction]
+    procedure = Procedure.new 'MyProc', args, empty_body
+
+    procedure.evaluate program_context, [Oeste.new]
+
+    program_context.has_variable_named?('a_direction').should be_false
+  end
+
   it "should fail if it is executed with more arguments than expected" do
     procedure = Procedure.new 'MyProcedure', empty_args, empty_body
     error_message = "Wrong number of arguments in procedure 'MyProcedure': expected 0, got 1"
