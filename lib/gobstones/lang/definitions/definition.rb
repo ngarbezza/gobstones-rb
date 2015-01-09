5cf7ad27d91225818dc4fc312b40939ee4848b66
Nahuel Garbezza <n.garbezza@gmail.com>
2015-01-09 07:16:05 -0300

    small renaming on definition object

--- lib/gobstones/lang/definitions/definition.rb
+++ lib/gobstones/lang/definitions/definition.rb
@@ -8,11 +8,11 @@ module Gobstones
 
       include Gobstones::EqualByClass
 
-      attr_reader :name, :args, :body, :return_statement
+      attr_reader :name, :arguments, :body, :return_statement
 
-      def initialize(name, args, body, return_statement)
+      def initialize(name, arguments, body, return_statement)
         @name = name
-        @args = args
+        @arguments = arguments
         @body = body
         @return_statement = return_statement
       end
@@ -20,7 +20,7 @@ module Gobstones
       def ==(other)
         super(other) &&
         self.name == other.name &&
-        self.args == other.args &&
+        self.arguments == other.arguments &&
         self.body == other.body &&
         self.return_statement == other.return_statement
       end
@@ -29,10 +29,10 @@ module Gobstones
         name == a_name
       end
 
-      def evaluate(context, arguments=[])
-        check_number_of_arguments arguments
+      def evaluate(context, calling_arguments=[])
+        check_number_of_arguments calling_arguments
         in_definition_context_based_on(context) do |definition_context|
-          set_arguments arguments, definition_context
+          set_arguments calling_arguments, definition_context
           body.evaluate definition_context
           return return_statement.evaluate definition_context
         end
@@ -48,21 +48,23 @@ module Gobstones
 
       private
 
-      def check_number_of_arguments(arguments)
-        raise Gobstones::Runner::WrongArgumentsError, wrong_number_of_arguments_message(arguments) if args.length != arguments.length
+      def check_number_of_arguments(calling_arguments)
+        raise Gobstones::Runner::WrongArgumentsError, wrong_number_of_arguments_message(calling_arguments) if
+            arguments.length != calling_arguments.length
       end
 
-      def wrong_number_of_arguments_message(arguments)
-        "Wrong number of arguments in #{definition_type} '#{name}': expected #{args.length}, got #{arguments.length}"
+      def wrong_number_of_arguments_message(calling_arguments)
+        "Wrong number of arguments in #{definition_type} '#{name}':" +
+            " expected #{arguments.length}, got #{calling_arguments.length}"
       end
 
       def in_definition_context_based_on(outer_context, &block)
         yield create_context_based_on(outer_context)
       end
 
-      def set_arguments(arguments, procedure_context)
-        args.length.times do |index|
-          procedure_context.set args.variables[index], arguments[index]
+      def set_arguments(calling_arguments, procedure_context)
+        arguments.length.times do |index|
+          procedure_context.set arguments.variables[index], calling_arguments[index]
         end
       end
     end
