2bafdb78dbdb30eea13bfb2213050215c6149eb4
Nahuel Garbezza <n.garbezza@gmail.com>
2015-01-11 14:41:41 -0300

    organize a bit requires

--- lib/gobstones/lang/all.rb
+++ lib/gobstones/lang/all.rb
@@ -1,5 +1,5 @@
-require 'gobstones/lang/literals/all'
-require 'gobstones/lang/expressions/all'
+require 'gobstones/lang/program'
 require 'gobstones/lang/commands/all'
 require 'gobstones/lang/definitions/all'
-require 'gobstones/lang/program'
+require 'gobstones/lang/expressions/all'
+require 'gobstones/lang/literals/all'
