4bcc9a7588cbe71e97ee429b415d5d46cd58579b
Nahuel Garbezza <n.garbezza@gmail.com>
2015-01-22 07:55:13 -0300

    little refactor on function call spec

--- spec/lang/expressions/function_call_spec.rb
+++ spec/lang/expressions/function_call_spec.rb
@@ -1,29 +1,39 @@
 describe FunctionCall do
 
-  it "evaluates an existing function and returns its result when calling it" do
-    return_statement = ReturnFromFunction.new [42.to_gbs_num]
-    my_function = Function.new 'myFunction', no_arguments, empty_body, return_statement
-    program = Program.new [my_function], no_return_statement
-    context = program_context_for program
-    call = FunctionCall.new 'myFunction', []
+  let(:my_function) { Function.new 'myFunction', args, empty_body, return_statement }
+  let(:program) { Program.new [my_function], no_return_statement }
+  let(:context) { program_context_for program }
+  let(:call) { FunctionCall.new 'myFunction', calling_arguments }
 
-    result = call.evaluate context
+  context "without arguments" do
 
-    expect(result).to eq(42.to_gbs_num)
+    let(:args) { no_arguments }
+    let(:return_statement) { ReturnFromFunction.new [42.to_gbs_num] }
+    let(:calling_arguments) { [] }
+
+    it "evaluates an existing function and returns its result when calling it" do
+      result = call.evaluate context
+
+      expect(result).to eq(42.to_gbs_num)
+    end
+
   end
 
-  it "evaluates an existing function with some arguments" do
-    number_a, number_b, number_c = 'a'.to_var_name, 'b'.to_var_name, 'c'.to_var_name
-    return_statement = ReturnFromFunction.new [Add.new(Add.new(number_a, number_b), number_c)]
-    args = VarTuple.new [number_a, number_b, number_c]
-    my_function = Function.new 'myFunction', args, empty_body, return_statement
-    program = Program.new [my_function], no_return_statement
-    context = program_context_for program
-    call = FunctionCall.new 'myFunction', [1.to_gbs_num, 2.to_gbs_num, 3.to_gbs_num]
+  context "with arguments" do
 
-    result = call.evaluate context
+    let(:number_a) { 'a'.to_var_name }
+    let(:number_b) { 'b'.to_var_name }
+    let(:number_c) { 'c'.to_var_name }
+    let(:args) { VarTuple.new [number_a, number_b, number_c] }
+    let(:return_statement) { ReturnFromFunction.new [Add.new(Add.new(number_a, number_b), number_c)] }
+    let(:calling_arguments) { [1.to_gbs_num, 2.to_gbs_num, 3.to_gbs_num] }
 
-    expect(result).to eq(6.to_gbs_num)
+    it "evaluates an existing function with some arguments" do
+      result = call.evaluate context
+
+      expect(result).to eq(6.to_gbs_num)
+    end
+
   end
 
 end
